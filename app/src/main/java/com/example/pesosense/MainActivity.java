10fc029227c6553ab442e41972fa1f1138c0b89e
addriadrie <143043838+addriadrie@users.noreply.github.com>
2024-06-25 10:12:27 +0800

    Add files via upload

--- app/src/main/java/com/example/pesosense/MainActivity.java
+++ app/src/main/java/com/example/pesosense/MainActivity.java
@@ -8,8 +8,10 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -31,6 +33,28 @@ public class MainActivity extends AppCompatActivity {
         scanButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, ScanActivity.class);
             startActivity(intent);
+        });
+
+        ImageButton forexButton = findViewById(R.id.coming_btn);
+        forexButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://wise.com/gb/currency-converter/usd-to-php-rate";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        ImageButton ncgButton = findViewById(R.id.ncg_btn);
+        ncgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://wise.com/gb/currency-converter/usd-to-php-rate";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
         });
 
     }
